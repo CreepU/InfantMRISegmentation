7ef0b415793ee0deb2f8ec8970829a4f3504dea5
CreepU <1113655836u@gmail.com>
2023-12-12 00:50:07 -0800

    Update ExtractFeature.py

--- ExtractFeature.py
+++ ExtractFeature.py
@@ -6,7 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-
 # normalize each itensity to unit l2 norm 
 
 def normalize_l2(image): 
@@ -22,29 +21,31 @@ def normalize_l2(image):
     return normalized_image
 
 
-def mean_intensity_difference(image, patch_size):
+# input data: patch_size_R is centered at the same 
+def mean_intensity_difference(image, patch_size_R, patch_size_R1,b=0):
+    len_R, len_R1 = (patch_size_R-1)/2, (patch_size_R1-1)/2
+    len_random = len_R-len_R1
     result = np.zeros_like(image, dtype=np.float32)
-    
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
             for k in range(image.shape[2]):
                 # Define cubical region R1 around voxel (i, j, k)
-                start_i1, start_j1, start_k1 = max(0, i - patch_size // 2), max(0, j - patch_size // 2), max(0, k - patch_size // 2)
-                end_i1, end_j1, end_k1 = min(image.shape[0], i + patch_size // 2 + 1), min(image.shape[1], j + patch_size // 2 + 1), min(image.shape[2], k + patch_size // 2 + 1)
-
+                center_i1 = np.random.randint(max(0, i - len_random), min(image.shape[0] - patch_size_R + 1, i + len_random + 1))
+                center_j1 = np.random.randint(j-len_random,j+len_random+1)
+                center_k1 = np.random.randint(k-len_random,k+len_random+1)
+                
                 # Define cubical region R2 (asymmetric to R1)
-                start_i2, start_j2, start_k2 = max(0, i - patch_size // 2), max(0, j - patch_size // 2), max(0, k - patch_size // 2)
-                end_i2, end_j2, end_k2 = min(image.shape[0], i + patch_size // 2, i + patch_size), min(image.shape[1], j + patch_size // 2, j + patch_size), min(image.shape[2], k + patch_size // 2, k + patch_size)
+                center_i2 = np.random.randint(i-len_random,i+len_random+1)
+                center_j2 = np.random.randint(j-len_random,j+len_random+1)
+                center_k2 = np.random.randint(k-len_random,k+len_random+1)
 
                 # Calculate mean intensity difference over R1 and R2
-                mean_diff = np.mean(image[start_i1:end_i1, start_j1:end_j1, start_k1:end_k1]) - np.mean(image[start_i2:end_i2, start_j2:end_j2, start_k2:end_k2])
+                mean_diff = np.mean(image[center_i1-len_R1:center_i1+len_R1+1, center_j1-len_R1:center_j1+len_R1+1, 
+                                        center_k1-len_R1:center_k1+len_R1+1]) 
+                - b*np.mean(image[center_i2-len_R1:center_i2+len_R1+1, center_j2-len_R1:center_j2+len_R1+1, 
+                                        center_k2-len_R1:center_k2+len_R1+1])
 
                 # Assign the mean intensity difference to the result
                 result[i, j, k] = mean_diff
-
+                
     return result
-
-def ExtractFeature(image,patch_size,b=0):
-    feature_image = np.zeros_like(image,dtype=np.float32)
-    
-    return 
